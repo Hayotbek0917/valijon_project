03fa9caf8b2fdabff7413869f0b0bbb7e72d0419
mahsudbekyuldoshev <mahsudbekyuldoshev09@gmail.com>
2026-06-17 18:53:25 +0500

    change auth

--- backend/apps/views/auth_views.py
+++ backend/apps/views/auth_views.py
@@ -1,4 +1,5 @@
-from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
@@ -7,11 +8,13 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from apps.serializers import RegisterModelSerializer, LoginModelSerializer
 
 
-class RegisterView(APIView):
+@extend_schema(tags=["auth"])
+class RegisterView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = RegisterModelSerializer
 
     def post(self, request):
-        serializer = RegisterModelSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(
@@ -20,13 +23,13 @@ class RegisterView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LoginView(APIView):
+@extend_schema(tags=["auth"])
+class LoginView(GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = LoginModelSerializer
 
     def post(self, request):
-        serializer = LoginModelSerializer(
-            data=request.data, context={"request": request}
-        )
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             user = serializer.validated_data["user"]
             refresh = RefreshToken.for_user(user)
